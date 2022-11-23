65df0e31daf63d982bf67a65320e656d511e17e1
DevExpressExampleBot <fakeEmail>
2022-11-24 01:31:08 +0400

    Source auto update [skip ci]

--- VB/XtraReport1.vb
+++ VB/XtraReport1.vb
@@ -1,54 +1,52 @@
-Imports Microsoft.VisualBasic
-#Region "#Usings"
+'#Region "#Usings"
 Imports System.Drawing
 Imports System.Drawing.Printing
 Imports DevExpress.XtraPrinting
 Imports DevExpress.XtraReports.UI
+
 ' ...
-#End Region ' #Usings
-
+'#End Region  ' #Usings
 Namespace ResetControlStyle
-	Partial Public Class XtraReport1
-		Inherits DevExpress.XtraReports.UI.XtraReport
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-#Region "#Code"
-Private Sub xrLabel2_BeforePrint(ByVal sender As Object, _ 
-ByVal e As PrintEventArgs) Handles xrLabel2.BeforePrint
-	SetCustomStyle(CType(sender, XRLabel))
-End Sub
+    Public Partial Class XtraReport1
+        Inherits XtraReport
 
-Private Sub xrLabel3_BeforePrint(ByVal sender As Object, _ 
-ByVal e As PrintEventArgs) Handles xrLabel3.BeforePrint
-	SetCustomStyle(CType(sender, XRLabel))
-	ResetStyle(CType(sender, XRLabel))
-End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-' Assign custom appearance settings to a control.
-Private Sub SetCustomStyle(ByVal label As XRLabel)
-	label.BackColor = Color.Gray
-	label.BorderColor = Color.DarkGray
-	label.Borders = BorderSide.All
-	label.BorderWidth = 0.5f
+'#Region "#Code"
+        Private Sub xrLabel2_BeforePrint(ByVal sender As Object, ByVal e As PrintEventArgs)
+            SetCustomStyle(CType(sender, XRLabel))
+        End Sub
 
-	label.Font = New Font(label.Parent.Font, FontStyle.Bold)
-	label.ForeColor = Color.White
-	label.TextAlignment = TextAlignment.MiddleRight
-End Sub
+        Private Sub xrLabel3_BeforePrint(ByVal sender As Object, ByVal e As PrintEventArgs)
+            SetCustomStyle(CType(sender, XRLabel))
+            ResetStyle(CType(sender, XRLabel))
+        End Sub
 
-' Restore the appearance settings to their default values.
-Private Sub ResetStyle(ByVal label As XRLabel)
-	label.ResetBackColor()
-	label.ResetBorderColor()
-	label.ResetBorders()
-	label.ResetBorderWidth()
-	label.ResetFont()
-	label.ResetForeColor()
-	label.ResetPadding()
-	label.ResetTextAlignment()
-End Sub
-#End Region ' #Code
-	End Class
+        ' Assign custom appearance settings to a control.
+        Private Sub SetCustomStyle(ByVal label As XRLabel)
+            label.BackColor = Color.Gray
+            label.BorderColor = Color.DarkGray
+            label.Borders = BorderSide.All
+            label.BorderWidth = 0.5F
+            label.Font = New Font(label.Parent.Font, FontStyle.Bold)
+            label.ForeColor = Color.White
+            label.TextAlignment = TextAlignment.MiddleRight
+        End Sub
+
+        ' Restore the appearance settings to their default values.
+        Private Sub ResetStyle(ByVal label As XRLabel)
+            label.ResetBackColor()
+            label.ResetBorderColor()
+            label.ResetBorders()
+            label.ResetBorderWidth()
+            label.ResetFont()
+            label.ResetForeColor()
+            label.ResetPadding()
+            label.ResetTextAlignment()
+        End Sub
+'#End Region  ' #Code
+    End Class
 End Namespace
